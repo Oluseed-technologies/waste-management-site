0f2ae7798589c6af1a28380f3b812aaaefe389f7
Halltech176 <devhalltech@gmail.com>
2022-09-26 04:52:42 -0700

    another

--- src/components/pages/Home/Header.jsx
+++ src/components/pages/Home/Header.jsx
@@ -1,16 +1,29 @@
 import pickUp from "./pickup.json";
+import { MdLocationOn, MdDirectionsCar, MdCalendarToday } from "react-icons/md";
 import tractor from "../../../assets/tractor1.png";
+import location from "../../../assets/location.png";
+import calendar from "../../../assets/calendar-2.png";
+import driving from "../../../assets/driving.png";
 import style from "./Header.module.css";
 import { motion } from "framer-motion";
 const Header = () => {
-  const renderPickups = pickUp.map((data) => {
+  const icons = [
+    <MdDirectionsCar />,
+    <MdLocationOn />,
+    <MdLocationOn />,
+    <MdCalendarToday />,
+  ];
+  const renderPickups = pickUp.map((data, index) => {
     return (
       <div className="flex flex-col ">
-        <label className="text-dark1 text-sm font-semibold" htmlFor={data.name}>
-          {data.name}
+        <label
+          className="text-dark1 text-sm flex items-center font-medium"
+          htmlFor={data.name}
+        >
+          <span className="mr-3 text-primary">{icons[index]}</span> {data.name}{" "}
         </label>
         <select
-          className="text-gray-600 border-solid border-2 rounded-md my-2 px-3 py-2 border-gray-100 text-sm"
+          className="text-gray-600 bg-white border-solid border-2 rounded-md my-2 px-3 py-2 border-gray-100 text-sm"
           name="type"
         >
           {data.options.map((option) => {
@@ -38,11 +51,14 @@ const Header = () => {
         className=""
         className={` shadow-lg  bg-white  0  text-lg p-7 `}
       >
-        <h2 className="font-bold">Schedule a pickup</h2>
+        <h2 className="font-medium text-xl">Schedule a pickup</h2>
         <p className="my-3 text-sm text-gray-600  font-rubik">
           Dispose your waste throuh hygenic means
         </p>
         {renderPickups}
+        <button className="w-full bg-primary rounded-md font-mont text-white py-3">
+          Fix Schedule
+        </button>
       </motion.section>
 
       <motion.section
